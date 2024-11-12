6e20c77da620a0416140a27d6d42175c236d12ce
fmael <mael.fath@ynov.com>
2024-11-12 15:09:59 +0100

    Created question timer

---
+++ quiz/src/components/Timer.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState, useRef } from 'react';
+
+interface Props {
+    timer: number;
+}
+
+const Timer: React.FC<Props> = ({ timer }) => {
+    const [time, setTime] = useState(timer);
+    const intervalRef = useRef<number | null>(null);
+
+    useEffect(() => {
+        setTime(timer);
+
+        intervalRef.current = window.setInterval(() => {
+            setTime((prevTime) => {
+                if (prevTime === 1) {
+                    clearInterval(intervalRef.current!);
+                    return 0;
+                }
+                return prevTime - 1;
+            });
+        }, 1000);
+
+        return () => {
+            if (intervalRef.current) clearInterval(intervalRef.current);
+        };
+    }, [timer]);
+
+    return (
+        <div>
+            <p>{time} s</p>
+        </div>
+    );
+};
+
+export default Timer;
