ae5b455ac220f9da1f6af7e0072918845336e03e
rthimoth <thimothee.ranvin@hotmail.com>
2024-11-12 10:58:59 +0100

    arborescence

---
+++ quiz/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Page d'accueil</h1>
+            <p>Bienvenue sur la page d'accueil !</p>
+        </div>
+    );
+};
+
+export default Home;
